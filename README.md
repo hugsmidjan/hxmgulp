0a8e2de6827dde4cd676160ce8300a222785e770
mar <mar.nospam@anomy.net>
2014-09-12 09:32:20 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+## Install:
+
+    npm install git+ssh://git@stash.hugsmidjan.is:7999/~mar/eplicagulp.git
+
+## Usage:
+
+In your project's skin folder create a `gulpfile.js` containing this command:
+
+    require('eplicagulp')(options);
+
+That's it.
+
+### Options (and their defaults):
+
+* `modules: ['/']` - array of (sub)folder names.
+* `src: '_src'` - path to the root source folder.
+* `dist: '.'` - path to the root distribution folder where the compiled/minified CSS and JS files are saved.
+* `cssProc: 'scss'` - type of CSS preporcessor being used. Currently valid options are `'scss'` and `'less'`
+* `task: null` - optional function which then gets run once for each item in the modules array.  
+  Example:  
+      tasks: function (data) {
+           // data.module ===  current module (i.e. (sub)folder)
+           // data.paths  ===  paths config for the current module
+           // data.basePathCfg === A good default options object for `gulp.task`
+           require('gulp').task('mytask', function(){ ... });
+           return ['mytask'];
+        }
+* `copyrightYear: (new Date()).getFullYear()` - Starting year for the copyright clauses at the top of minified files.
+* `copyrightInfo: 'Hugsmiðjan evhf. (www.hugsmidjan.is)'` - text that appears after "Copyright 20XX-20YY ..." in the copyright clause.
+
+
---
+++ index.js
@@ -0,0 +1,268 @@
+module.exports = function (skin) {
+    var pkg = require('./package.json');
+
+    var gulp = require('gulp');
+
+    var fs = require('fs');
+    var path = require('path');
+    var https = require('https');
+
+    var plumber = require('gulp-plumber');
+    var filter = require('gulp-filter');
+    var changed = require('gulp-changed');
+    var replace = require('gulp-replace');
+    var header = require('gulp-header');
+    var rename = require('gulp-rename');
+
+    var isLESS = skin.cssProc==='less';
+    var isSCSS = !isLESS;
+    var less = isLESS && require('gulp-less');
+    var scss = isSCSS && require('gulp-ruby-sass');
+
+    var autoprefixer = require('gulp-autoprefixer');
+    var datauri = require('gulp-base64');
+    var minifycss = require('gulp-minify-css');
+
+    var imagemin = require('gulp-imagemin');
+    var iconfont = require('gulp-iconfont');
+
+    var browserify = require('gulp-browserify');
+    var uglify = require('gulp-uglify');
+
+    var nunjucksRender = require('gulp-nunjucks-render');
+
+
+
+    // =========================================================================
+
+
+    var nowYear = new Date().getFullYear();
+    var copyrightBanner = 'Copyright '+(skin.copyrightYear||nowYear)+ (2014<nowYear?'-'+nowYear:'') +' '+(skin.copyrightInfo||pkg.author)+'\n';
+
+    var buildTasks =    [];
+    var watchTasks =    [];
+    var htmltestTasks = [];
+    var nunjucksWorkingDirs = [];
+
+    if ( isSCSS )
+    {
+      // curl codecentre files...
+      var ccFolder = '_src/_scss/_codecentre/';
+      if ( !fs.existsSync(ccFolder) ){  fs.mkdirSync( ccFolder );  }
+      ['utils.scss','eplica-utils.scss','normalize.scss']
+          .forEach(function (fileName) {
+              https.get('https://codecentre.eplica.is/themes/scss/'+fileName, function (res) {
+                  res.pipe( fs.createWriteStream( ccFolder+fileName ) );
+                });
+            });
+    }
+
+    var cssGlob = '*.'+skin.cssProc;
+    var skinModules = skin.modules || ['/'];
+    var skinSrc = (skin.src || '_src').replace(/\/+$/,'');
+    var skinDist = (skin.dist || '.').replace(/\/+$/,'');
+
+
+    skinModules.forEach(function (module, folderIndex) {
+
+        module = ('/'+module+'/').replace(/\/\/+/g, '/'); // normalize
+        var srcPath = skinSrc + module;
+        var paths = {
+                src:          srcPath,
+                dist:         skinDist + module,
+
+                css:         srcPath + '',
+                css_incl:    isSCSS ? '_scss/' : '_less',
+                scripts:      srcPath + '',
+                scripts_incl: '_js/',
+                images:       srcPath + 'i/',
+                iconfont:     srcPath + 'iconfont/',
+                htmltests:    srcPath + '_tests/',
+              };
+
+        var basePathCfg = { base:paths.src };
+        var ns = module;
+
+        // ==============================================
+
+        buildTasks.push( ns+'iconfont' );
+        gulp.task(ns+'iconfont', function() {
+            return gulp.src([
+                paths.iconfont + '**/*.svg',
+                '!' + paths.iconfont + '_raw/**'
+              ], basePathCfg )
+                .pipe( plumber() )
+                .pipe( iconfont({
+                    fontName:   'icons',
+                    normalize:  true
+                  }) )
+                .on('codepoints', function (codepoints/*, options*/) {
+                    var iconData = [];
+                    var iconVars = [];
+                    codepoints.forEach(function (cp) {
+                        var name = cp.name;
+                        var chr = '"\\' + cp.codepoint.toString(16) + '"';
+                        var padding = new Array(Math.max(20 - name.length,2)).join(' ');
+                        iconVars.push( (isSCSS?'$':'@')+'icon-' + name + ':' + padding + chr + ';\n' );
+                        isSCSS && iconData.push( '(' + name + ', ' + chr + ')' );
+                      });
+                    var code = '// This file is auto-generated. DO NOT EDIT! \n\n' +
+                                iconVars.join('') + '\n' +
+                                (isSCSS && '$iconData:\n    ' + iconData.join(',\n    ') + ';\n\n');
+                    fs.writeFileSync( paths.css + paths.css_incl + '_iconVars.'+skin.cssProc, code );
+                  })
+                .pipe( gulp.dist( paths.dist + path.relative(paths.src,paths.images) ) );
+          });
+
+
+        buildTasks.push( ns+'images' );
+        gulp.task(ns+'images', function() {
+            var imgFilter = filter('**/*.{png,gif,jpg,jpeg}');
+            return gulp.src([
+                paths.images + '**/*.*',
+                '!' + paths.images + '_raw/**'
+              ], basePathCfg )
+                .pipe( plumber() )
+                .pipe( changed( paths.dist ) )
+                .pipe( imgFilter )
+                    .pipe( imagemin() )
+                    .pipe( imgFilter.restore() )
+                .pipe( gulp.dist( paths.dist ) );
+          });
+
+
+        buildTasks.push( ns+'scripts' );
+        gulp.task(ns+'scripts', function() {
+            var commonjsScripts = filter('**/*-CommonJS.js');
+            return gulp.src([ paths.scripts+'*.js'], basePathCfg )
+                .pipe( commonjsScripts )
+                    .pipe( browserify() )
+                    .pipe( rename(function(path){ path.basename = path.basename.replace(/-CommonJS$/, '');  }) )
+                    .pipe( commonjsScripts.restore() )
+                .pipe( rename({ suffix:'-source' }) )
+                .pipe( gulp.dist( paths.dist ) )
+
+                .pipe( uglify() )
+                .pipe( header('// '+copyrightBanner) )
+                .pipe( rename(function(path){ path.basename = path.basename.replace(/-source$/, '');  }) )
+                .pipe( gulp.dist( paths.dist ) );
+          });
+
+
+        buildTasks.push( ns+'css' );
+        gulp.task(ns+'css', function() {
+            return gulp.src( paths.css+cssGlob, basePathCfg )
+                .pipe( plumber() )
+                .pipe( isSCSS ?
+                    scss({
+                        precision:7,
+                        'sourcemap=none':true, // dodgy temporary workaround
+                        // sourcemap:'none',
+                        container:'gulp-ruby-sass-'+folderIndex, // Workaround for https://github.com/sindresorhus/gulp-ruby-sass/issues/124#issuecomment-54682317
+                      }):
+                    less(/*{ strictMath: true }*/)
+                 )
+                .pipe( autoprefixer('> 0.5%', 'last 2 versions', 'Firefox ESR', 'Opera 12.1'/*, { cascade: true }*/) )
+                .pipe( minifycss({
+                    noAdvanced:true, // turn off advanced/aggressive merging. It's too buggy still. Ack!
+                    keepBreaks:true,
+                    compatibility:'ie8'
+                  }) )
+                .pipe( replace(/ -no-merge/g,'') )
+                .pipe( datauri({
+                    baseDir: paths.dist,
+                    extensions: [ (/#datauri$/) ],
+                    // maxImageSize: bytes,
+                    // debug: true,
+                  }) )
+                .pipe( gulp.dist( paths.dist ) );
+          });
+
+
+
+        htmltestTasks.push( ns+'htmltests-html' );
+        nunjucksWorkingDirs.push( paths.htmltests );
+        gulp.task(ns+'htmltests-html', function() {
+            var nonHTMLFiles = filter('*.*.html'); // <-- because nunjucksRender renames all files to .html
+            var nonHTMLFileDir = path.relative(paths.htmltests, paths.src);
+            return gulp.src( paths.htmltests+'*.htm', basePathCfg )
+                .pipe( plumber() )
+                .pipe( nunjucksRender() )
+                .pipe( replace(/^[\s*\n]+/, '') ) // remove macro/config induced whitespace at start of file.
+                .pipe( nonHTMLFiles )
+                    .pipe( rename({ extname:'', dirname:nonHTMLFileDir  }) )
+                    .pipe( nonHTMLFiles.restore() )
+                .pipe( gulp.dist( paths.dist ) );
+          });
+        htmltestTasks.push( ns+'htmltests-images' );
+        gulp.task(ns+'htmltests-images', function() {
+            return gulp.src( paths.htmltests+'media/**/*.*', basePathCfg )
+                .pipe( plumber() )
+                .pipe( changed( paths.dist ) )
+                    .pipe( imagemin() )
+                    .pipe( gulp.dist( paths.dist ) );
+          });
+        buildTasks.push( ns+'htmltests-scripts' );
+        gulp.task(ns+'htmltests-scripts', function() {
+            return gulp.src([ paths.htmltests+'*.js'], basePathCfg )
+                .pipe( browserify() )
+                .pipe( gulp.dist( paths.dist ) );
+          });
+
+
+
+        var buildGlobs = function (workPath, glob, inclGlob) {
+                var globs = [workPath + glob];
+                var relativePath = path.relative( paths.src , workPath );
+                inclGlob =  relativePath ?
+                                relativePath + '/' + inclGlob:
+                                inclGlob;
+                var folder = paths.src;
+                while ( folder )
+                {
+                    globs.push( folder + inclGlob );
+                    folder = folder.replace(/[^\/]+\/$/,'');
+                }
+                return globs;
+              };
+
+        watchTasks.push(ns+'watch');
+        gulp.task(ns+'watch', function() {
+            gulp.watch(buildGlobs(paths.css, cssGlob, paths.css_incl+'**/'+cssGlob),   [ns+'css']);
+            gulp.watch(buildGlobs(paths.scripts,'*.js', paths.scripts_incl+'**/*.js'), [ns+'scripts']);
+            gulp.watch([ paths.images+'**/*', '!'+paths.images+'_raw/**'],        [ns+'images']);
+            gulp.watch([ paths.iconfont+'**/*', '!'+paths.iconfont+'_raw/**/*'],  [ns+'iconfont']);
+            gulp.watch([ paths.htmltests+'**/*.htm'],                             [ns+'htmltests-html']);
+            gulp.watch([ paths.htmltests+'media/**/*'],                           [ns+'htmltests-images']);
+            gulp.watch([ paths.htmltests+'**/*.js'],                              [ns+'htmltests-scripts']);
+          });
+
+
+        // allow skinfile.js to define its own tasks
+        if ( typeof skin.tasks === 'function' )
+        {
+            var taskNames = skin.tasks({
+                                module: module,
+                                paths:  paths,
+                                basePathCfg: Object.create(basePathCfg),
+                              });
+            if ( taskNames )
+            {
+                buildTasks.push.apply(buildTasks, taskNames);
+            }
+        }
+
+      });
+
+    nunjucksRender.nunjucks.configure(nunjucksWorkingDirs);
+
+
+    gulp.task('htmltests', htmltestTasks);
+    buildTasks.unshift('htmltests');
+    gulp.task('build', buildTasks);
+    gulp.task('watch', watchTasks);
+
+    gulp.task('default', ['build', 'watch']);
+
+
+  };
